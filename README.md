7482267b3a3e2f6d9d93cc3833bfac478e958ed0
poeynus <poeynus@gmail.com>
2024-04-05 16:30:49 +0900

    READMODE 수정

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # tarkov-korea-wiki-back
 
+DB 조회 모델 상위 클래스 생성 하고 하위 클래스 만들기 
+
+=> 함수에 클래스랑 쿼리를 매개변수로 보내서 dict를 만들어서 줄 건데, 이 때 사용할 것 
+
 pip3 install 'fastapi[all]'
 
 pip3 install sqlalchemy
--- src/database.py
+++ src/database.py
@@ -7,6 +7,7 @@ class DataBaseConnector:
     """
     Database 연결 후 쿼리 전송
     결과를 tuple List에 담아서 반환 => [(1, a, 2), (2, b, 3)]
+    orm을 사용하지 않고 쿼리를 직접 작성할 것이기에 session 미사용
     """
     @classmethod
     def query_sender(cls, query):
