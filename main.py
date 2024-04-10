fcf954dd7414f11c9b92e2267c5e22502b682bef
poeynus <poeynus@gmail.com>
2024-04-10 18:41:29 +0900

    CORS 문제 해결

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.openapi.docs import (
     get_swagger_ui_html,
 )
 from router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title='tarkov-korea-wiki-back',
@@ -41,3 +42,11 @@ def root():
 
 
 app.include_router(api_router, prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True, # cookie 포함 여부를 설정한다. 기본은 False
+    allow_methods=["*"],    # 허용할 method를 설정할 수 있으며, 기본값은 'GET'이다.
+    allow_headers=["*"],	# 허용할 http header 목록을 설정할 수 있으며 Content-Type, Accept, Accept-Language, Content-Language은 항상 허용된다.
+)
