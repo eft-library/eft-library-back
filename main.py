0a998dcfa2aa593e1f117855fbf2681cca62b42d
poeynus <poeynus@gmail.com>
2024-04-12 16:15:33 +0900

    배포 방식 변경 - 하나로 합치기

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from fastapi.openapi.docs import (
-    get_redoc_html,
-    get_swagger_ui_html,
-)
-from api.router import api_router
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
+from api.router import api_router
+from starlette.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi.openapi.docs import get_swagger_ui_html
 
 load_dotenv()
 
@@ -15,42 +13,29 @@ app = FastAPI(
     title='tarkov-korea-wiki-back',
     openapi_url=f"/api/v1/openapi.json",
     docs_url=None,
-    redoc_url=None)
+    redoc_url=None,
+)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# 정적 파일 서빙 설정
+app.mount("/static", StaticFiles(directory="ui/build/static"), name="static")
 
-
-@app.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - Swagger UI",
-        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="/static/swagger-ui-bundle.js",
-        swagger_css_url="/static/swagger-ui.css",
-    )
-
-
-@app.get("/redoc", include_in_schema=False)
-async def redoc_html():
-    return get_redoc_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - ReDoc",
-        redoc_js_url="/static/redoc.standalone.js",
-    )
-
-
+# React 애플리케이션의 index.html을 반환하는 라우터 추가
 @app.get("/")
-def root():
-    return {"message": "hi"}
+async def serve_react_app():
+    return FileResponse("ui/build/index.html")
 
+# Swagger UI 제공을 위한 엔드포인트
+@app.get("/docs")
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(openapi_url="/api/v1/openapi.json", title="Swagger UI")
 
 app.include_router(api_router, prefix=os.getenv("API_PREFIX"))
 
+# CORS 미들웨어 추가
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True, # cookie 포함 여부를 설정한다. 기본은 False
-    allow_methods=["*"],    # 허용할 method를 설정할 수 있으며, 기본값은 'GET'이다.
-    allow_headers=["*"],	# 허용할 http header 목록을 설정할 수 있으며 Content-Type, Accept, Accept-Language, Content-Language은 항상 허용된다.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
