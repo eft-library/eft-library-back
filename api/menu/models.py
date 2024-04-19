69f22adcffa8b3c59288e1d4cab2e0553e6d5157
poeynus <poeynus@gmail.com>
2024-04-19 09:16:31 +0900

    menu api 수정 - info 이미지 추가

--- api/menu/models.py
+++ api/menu/models.py
@@ -15,8 +15,9 @@ class MainMenu(DataBaseConnector.Base):
     main_menu_kr_name = Column(String)
     main_menu_link = Column(String)
     main_menu_order = Column(INT)
+    main_menu_image = Column(String)
     main_menu_update_time = Column(TIMESTAMP)
-    sub_menus = relationship("SubMenu", backref="main_menu")
+    sub_menus = relationship("SubMenu", backref="main_menu", order_by='SubMenu.sub_menu_order')
 
 
 class SubMenu(DataBaseConnector.Base):
@@ -30,4 +31,6 @@ class SubMenu(DataBaseConnector.Base):
     sub_menu_kr_name = Column(String)
     sub_menu_parent_value = Column(String, ForeignKey('tkw_main_menu_info.main_menu_value'))
     sub_menu_link = Column(String)
+    sub_menu_order = Column(String)
+    sub_menu_image = Column(String)
     sub_menu_update_time = Column(TIMESTAMP)
--- api/menu/service.py
+++ api/menu/service.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, subqueryload
 from api.menu.models import MainMenu, SubMenu
 from database import DataBaseConnector
 
@@ -7,6 +7,9 @@ class MenuService:
     @staticmethod
     def get_all_menu():
         session = DataBaseConnector.create_session()
-        main_menu_list = session.query(MainMenu).options(joinedload(MainMenu.sub_menus)).order_by(MainMenu.main_menu_order).all()
+        main_menu_list = (session
+                          .query(MainMenu)
+                          .options(subqueryload(MainMenu.sub_menus))
+                          .order_by(MainMenu.main_menu_order).all())
         session.close()
         return main_menu_list
