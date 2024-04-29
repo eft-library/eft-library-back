2f14fc3c9adc945eb6abe6ee5d7f9999ea79191c
poeynus <poeynus@gmail.com>
2024-04-29 11:11:04 +0900

    새로고침 대으

--- main.py
+++ main.py
@@ -9,25 +9,30 @@ from fastapi.openapi.docs import get_swagger_ui_html
 
 load_dotenv()
 
-app = FastAPI(
-    title='tarkov-korea-wiki-back',
-    openapi_url=f"/api/v1/openapi.json",
-    docs_url=None,
-    redoc_url=None,
-)
+app = FastAPI(title='tarkov-korea-wiki-back')
 
 # 정적 파일 서빙 설정
 app.mount("/static", StaticFiles(directory="ui/build/static"), name="static")
 
+
 # React 애플리케이션의 index.html을 반환하는 라우터 추가
 @app.get("/")
+@app.get("/map/{path:path}")
+@app.get("/quest/{path:path}")
+@app.get("/item/{path:path}")
+@app.get("/boss/{path:path}")
+@app.get("/hideout/{path:path}")
 async def serve_react_app():
     return FileResponse("ui/build/index.html")
+
 
 # Swagger UI 제공을 위한 엔드포인트
 @app.get("/docs")
 async def custom_swagger_ui_html():
-    return get_swagger_ui_html(openapi_url="/api/v1/openapi.json", title="Swagger UI")
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",  # FastAPI 자동 생성된 OpenAPI 스키마 경로
+        title="Swagger UI"
+    )
 
 app.include_router(api_router, prefix=os.getenv("API_PREFIX"))
 
