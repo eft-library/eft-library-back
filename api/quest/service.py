cbaaa16e72e285b26721e524183976b535890436
poeynus <poeynus@gmail.com>
2024-05-02 13:50:06 +0900

    Quest 상세 페이지 조회 link 주소 로직  수정

--- api/quest/service.py
+++ api/quest/service.py
@@ -1,5 +1,7 @@
 from api.quest.models import NPC, QuestPreview
 from database import DataBaseConnector
+import os
+from dotenv import load_dotenv
 
 
 class QuestService:
@@ -23,12 +25,14 @@ class QuestService:
 
     @staticmethod
     def get_quest_by_id(quest_id):
+        load_dotenv()
         session = DataBaseConnector.create_session()
         quest_npc_info = (session
-                 .query(QuestPreview, NPC)
-                 .filter(QuestPreview.quest_npc_value == NPC.npc_id)
-                 .filter(QuestPreview.quest_id == quest_id)
-                 .first())
+                          .query(QuestPreview, NPC)
+                          .filter(QuestPreview.quest_npc_value == NPC.npc_id)
+                          .filter(QuestPreview.quest_id == quest_id)
+                          .first())
+        quest_npc_info[0].quest_guide = quest_npc_info[0].quest_guide.replace("/tkw_quest", os.getenv("NAS_DATA") + "/tkw_quest")
         combined_info = {**quest_npc_info[0].__dict__, **quest_npc_info[1].__dict__}
         session.close()
         return combined_info
