9b362d42335636fb926486d5490de6f775f16e2d
poeynus <poeynus@gmail.com>
2024-05-21 10:48:32 +0900

    weapon 통신 완료

--- api/weapon/models.py
+++ api/weapon/models.py
@@ -1,11 +1,12 @@
 from database import DataBaseConnector
-from sqlalchemy import Column, Integer, TIMESTAMP, ARRAY, TEXT
+from sqlalchemy import Column, Integer, TIMESTAMP, ARRAY, TEXT, NUMERIC
 
 
 class Weapon(DataBaseConnector.Base):
     """
-    Weapon
+    Weapon Gun
     """
+
     __tablename__ = "tkw_weapon_info"
 
     weapon_id = Column(TEXT, primary_key=True)
@@ -24,4 +25,38 @@ class Weapon(DataBaseConnector.Base):
     weapon_update_time = Column(TIMESTAMP)
 
 
+class Knife(DataBaseConnector.Base):
+    """
+    Weapon Knife
+    """
 
+    __tablename__ = "tkw_knife_info"
+
+    knife_id = Column(TEXT, primary_key=True)
+    knife_name = Column(TEXT)
+    knife_short_name = Column(TEXT)
+    knife_image = Column(TEXT)
+    knife_category = Column(TEXT)
+    knife_slash_damage = Column(Integer)
+    knife_stab_damage = Column(Integer)
+    knife_hit_radius = Column(NUMERIC)
+    knife_update_time = Column(TIMESTAMP)
+
+
+class Throwable(DataBaseConnector.Base):
+    """
+    Weapon Throwable
+    """
+
+    __tablename__ = "tkw_throwable_info"
+
+    throwable_id = Column(TEXT, primary_key=True)
+    throwable_name = Column(TEXT)
+    throwable_short_name = Column(TEXT)
+    throwable_image = Column(TEXT)
+    throwable_category = Column(TEXT)
+    throwable_fuse = Column(NUMERIC)
+    throwable_min_explosion_distance = Column(Integer)
+    throwable_max_explosion_distance = Column(Integer)
+    throwable_fragments = Column(Integer)
+    throwable_update_time = Column(TIMESTAMP)
--- api/weapon/service.py
+++ api/weapon/service.py
@@ -1,4 +1,4 @@
-from api.weapon.models import Weapon
+from api.weapon.models import Weapon, Knife, Throwable
 from database import DataBaseConnector
 
 
@@ -6,9 +6,13 @@ class WeaponService:
     @staticmethod
     def get_all_weapon():
         session = DataBaseConnector.create_session()
-        weapon_list = (session
-                        .query(Weapon)
-                        .all())
+        gun_list = session.query(Weapon).all()
+        knife_list = session.query(Knife).all()
+        throwable_list = session.query(Throwable).all()
+        weapon_list = {
+            "gun": gun_list,
+            "knife": knife_list,
+            "throwable": throwable_list,
+        }
         session.close()
         return weapon_list
-
