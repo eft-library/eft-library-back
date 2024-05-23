fe1dd14c7836a0890c99c9b05392d1e51a690b7e
poeynus <poeynus@gmail.com>
2024-05-23 09:34:25 +0900

    boss img return example add

--- api/boss/service.py
+++ api/boss/service.py
@@ -1,5 +1,7 @@
 from api.boss.models import Boss
 from database import DataBaseConnector
+import os
+from dotenv import load_dotenv
 
 
 class BossService:
@@ -9,8 +11,21 @@ class BossService:
         boss 전체 조회
         """
         try:
+            load_dotenv()
             session = DataBaseConnector.create_session()
             boss_list = session.query(Boss).all()
+
+            # updated_boss_list = []
+            # for boss in boss_list:
+            #     if boss.boss_location_guide is not None:
+            #         boss.boss_location_guide = boss.boss_location_guide.replace(
+            #             "/tkw_quest", "/tkw_quest"
+            #         )
+            #     updated_boss_list.append(boss)
+            #
+            # # 각각의 Boss 객체를 딕셔너리로 변환
+            # combined_info = [boss.__dict__ for boss in updated_boss_list]
+
             session.close()
             return boss_list
         except Exception as e:
