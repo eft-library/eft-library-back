a3f289a8ea283016ad519b04e9184696c5a3f998
poeynus <poeynus@gmail.com>
2024-05-23 09:36:51 +0900

    boss img return enavle

--- api/boss/service.py
+++ api/boss/service.py
@@ -15,19 +15,19 @@ class BossService:
             session = DataBaseConnector.create_session()
             boss_list = session.query(Boss).all()
 
-            # updated_boss_list = []
-            # for boss in boss_list:
-            #     if boss.boss_location_guide is not None:
-            #         boss.boss_location_guide = boss.boss_location_guide.replace(
-            #             "/tkw_quest", "/tkw_quest"
-            #         )
-            #     updated_boss_list.append(boss)
-            #
-            # # 각각의 Boss 객체를 딕셔너리로 변환
-            # combined_info = [boss.__dict__ for boss in updated_boss_list]
+            updated_boss_list = []
+            for boss in boss_list:
+                if boss.boss_location_guide is not None:
+                    boss.boss_location_guide = boss.boss_location_guide.replace(
+                        "/tkw_quest", "/tkw_quest"
+                    )
+                updated_boss_list.append(boss)
+
+            # 각각의 Boss 객체를 딕셔너리로 변환
+            combined_info = [boss.__dict__ for boss in updated_boss_list]
 
             session.close()
-            return boss_list
+            return combined_info
         except Exception as e:
             print("오류 발생:", e)
             return None
