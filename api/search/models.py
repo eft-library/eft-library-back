fb613aa3d4dd3c025ec9d122e2af7d429fe99950
poeynus <poeynus@gmail.com>
2024-07-03 20:05:42 +0900

    search 데이터 수정

--- api/search/models.py
+++ api/search/models.py
@@ -10,8 +10,7 @@ class Search(DataBaseConnector.Base):
 
     __tablename__ = "tkl_search"
 
-    id = Column(String, primary_key=True)
-    value = Column(String)
+    value = Column(String, primary_key=True)
     link = Column(String)
     order = Column(INTEGER)
     page_value = Column(INTEGER)
