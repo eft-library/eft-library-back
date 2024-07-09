22f034df701b544a739f9421f4fd13d8beb1afc4
poeynus <poeynus@gmail.com>
2024-07-09 09:31:25 +0900

    오타 수정

--- api/item/service.py
+++ api/item/service.py
@@ -277,12 +277,12 @@ class ItemService:
                            tkl_loot.image,
                            COALESCE(jsonb_agg(distinct
                                     jsonb_build_object('id', rq, 'name', tkl_quest.name_en, 'name_kr', tkl_quest.name_kr, 'in_raid',
-                                                        tkl_related_quest.in_raid, 'count', tkl_related_quest."count"))
+                                                       tkl_related_quest.in_raid, 'count', tkl_related_quest."count"))
                                     FILTER (WHERE rq IS NOT NULL), '[]'::jsonb) as notes
                     from tkl_loot
                              LEFT JOIN LATERAL unnest(tkl_loot.related_quests) AS rq ON true
-                             LEFT JOIN tkl_quest on tkl_quest.id = rq
-                             LEFT JOIN tkl_related_quest on tkl_loot.id = tkl_related_quest.item_id and tkl_quest.id = rq
+                             LEFT JOIN tkl_related_quest on rq = tkl_related_quest.quest_id and tkl_loot.id = tkl_related_quest.item_id
+                             LEFT JOIN tkl_quest on rq = tkl_quest.id
                     group by tkl_loot.id, tkl_loot.name_en, tkl_loot.name_kr, tkl_loot.short_name,
                              tkl_loot.category, tkl_loot.image
                     """
