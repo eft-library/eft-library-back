e15aed245579bb58217d63c908e67a8794237c8e
poeynus <poeynus@gmail.com>
2024-07-19 15:07:06 +0900

    event api 추가

--- api/constants.py
+++ api/constants.py
@@ -8,6 +8,7 @@ class Message(Enum):
     MENU_NOT_FOUND = "Menu not found"
     NPC_NOT_FOUND = "NPC not found"
     QUEST_NOT_FOUND = "Quest not found"
+    EVENT_NOT_FOUND = "Event not found"
     WEAPON_NOT_FOUND = "Weapon not found"
     MAIN_INFO_NOT_FOUND = "Main info not found"
     BOSS_NOT_FOUND = "Boss not found"
