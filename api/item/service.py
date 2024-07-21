ab0f988e0145b21a4e7dad9e817e7d2e9195f832
poeynus <poeynus@gmail.com>
2024-07-21 14:30:25 +0900

    loot hideout 연관 정보 추가

--- api/item/service.py
+++ api/item/service.py
@@ -282,11 +282,18 @@ class ItemService:
                            COALESCE(jsonb_agg(distinct
                                     jsonb_build_object('id', rq, 'name', tkl_quest.name_en, 'name_kr', tkl_quest.name_kr, 'in_raid',
                                                        tkl_related_quest.in_raid, 'count', tkl_related_quest."count"))
-                                    FILTER (WHERE rq IS NOT NULL), '[]'::jsonb) as notes
+                                    FILTER (WHERE rq IS NOT NULL), '[]'::jsonb)  as quest_notes,
+                           COALESCE(jsonb_agg(distinct
+                                    jsonb_build_object('id', hid, 'master_id', tkl_related_hideout.hideout_master_id, 'name',
+                                                       tkl_related_hideout.hideout_master_name_en, 'name_kr',
+                                                       tkl_related_hideout.hideout_master_name_kr, 'count', tkl_related_hideout."count"))
+                                    FILTER (WHERE hid IS NOT NULL), '[]'::jsonb) as hideout_notes
                     from tkl_loot
                              LEFT JOIN LATERAL unnest(tkl_loot.related_quests) AS rq ON true
                              LEFT JOIN tkl_related_quest on rq = tkl_related_quest.quest_id and tkl_loot.id = tkl_related_quest.item_id
                              LEFT JOIN tkl_quest on rq = tkl_quest.id
+                             LEFT JOIN LATERAL unnest(tkl_loot.related_hideout) AS hid on true
+                             LEFT JOIN tkl_related_hideout on hid = tkl_related_hideout.hideout_sub_id
                     group by tkl_loot.id, tkl_loot.name_en, tkl_loot.name_kr, tkl_loot.short_name,
                              tkl_loot.category, tkl_loot.image
                     """
@@ -302,6 +309,7 @@ class ItemService:
                         "category": row[4],
                         "image": row[5],
                         "related_quests": row[6],
+                        "related_hideout": row[7]
                     }
                     loot.append(loot_dict)
                 return loot
