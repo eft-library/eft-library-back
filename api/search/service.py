a35813029b40506fcb36dd271417e9033b5aab44
poeynus <poeynus@gmail.com>
2024-07-22 10:05:18 +0900

    sitemap api 수정

--- api/search/service.py
+++ api/search/service.py
@@ -1,5 +1,8 @@
 from api.search.models import Search
 from database import DataBaseConnector
+import os
+from dotenv import load_dotenv
+from sqlalchemy import text
 
 
 class SearchService:
@@ -22,11 +25,37 @@ class SearchService:
         """
         사이트 정보 전체 조회
         """
+        load_dotenv()
+
         try:
             session = DataBaseConnector.create_session_factory()
             with session() as s:
-                search_list = s.query(Search).with_entities(Search.link).distinct().order_by(Search.link).all()
-                return [link for link, in search_list]
+                query = text(
+                    """
+                    select '/icon.ico' as tkl_link
+                    union all
+                    select '/privacy' as tkl_link
+                    union all
+                    select '/terms' as tkl_link
+                    union all
+                    SELECT DISTINCT tkl_search.link AS tkl_link
+                    FROM tkl_search
+                    union all
+                    select link AS tkl_link
+                    from tkl_main
+                    where value != 'MAP_OF_TARKOV'
+                      and value != 'INTERACTIVE_MAPS'
+                    order by tkl_link
+                    """
+                )
+                result = s.execute(query)
+
+                search_list = []
+
+                for row in result:
+                    search_list.append(os.getenv("SITE_URL") + row[0])
+
+                return search_list
         except Exception as e:
             print("오류 발생:", e)
             return None
