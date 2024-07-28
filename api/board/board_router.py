e752aaede52fea7bd1cb5c0b55f5fa38ef2bff5a
poeynus <poeynus@gmail.com>
2024-07-29 08:34:23 +0900

    ㅇㅣ미지 주소 추가

--- api/board/board_router.py
+++ api/board/board_router.py
@@ -17,5 +17,7 @@ def upload_image(file: UploadFile = File(...)):
     file_location, unique_filename = BoardService.save_file(file, file.filename)
     # 로직 변경 -> scp로 파일 전달에서 바로 저장공간을 연동으로 변경
     # if BoardService.upload_to_remote(file_location, unique_filename):
-    image_url = f"{os.getenv('BOARD_IMAGE_PATH')}/{unique_filename}"
+    image_url = (
+        f"{os.getenv('NAS_DATA')}{os.getenv('BOARD_IMAGE_PATH')}/{unique_filename}"
+    )
     return CustomResponse.response(image_url, HTTPCode.OK, Message.SUCCESS)
