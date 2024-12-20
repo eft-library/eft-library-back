f2ce3cf42b4a6713eb26fb5b1e813b63ba27d756
poeynus <poeynus@gmail.com>
2024-12-20 09:04:23 +0900

    user quest require 추가

--- api/user/util.py
+++ api/user/util.py
@@ -64,13 +64,16 @@ class UserUtil:
         """
 
         return """
-                select tkl_npc.id                                             as npc_id,
-                       tkl_npc.name_kr                                        as npc_name_kr,
-                       tkl_npc.name_en                                        as npc_name_en,
-                       tkl_npc.image                                          as npc_image,
+                select tkl_npc.id                                                                                                  as npc_id,
+                       tkl_npc.name_kr                                                                                             as npc_name_kr,
+                       tkl_npc.name_en                                                                                             as npc_name_en,
+                       tkl_npc.image                                                                                               as npc_image,
                        jsonb_agg(jsonb_build_object('quest_id', rq, 'quest_name_en', tkl_quest.name_en, 'quest_name_kr',
-                                                    tkl_quest.name_kr, 'objectives_kr', tkl_quest.objectives_kr, 'objectives_en',
-                                                    tkl_quest.objectives_en, 'next', COALESCE(tkl_quest.next, jsonb '[]'::jsonb))) as quest_info
+                                                    tkl_quest.name_kr, 'requirements_kr', tkl_quest.requirements_kr, 'requirements_en',
+                                                    tkl_quest.requirements_en, 'objectives_kr', tkl_quest.objectives_kr,
+                                                    'objectives_en',
+                                                    tkl_quest.objectives_en, 'next',
+                                                    COALESCE(tkl_quest.next, jsonb '[]'::jsonb)))                                  as quest_info
                 from tkl_user_quest
                          left join lateral unnest(tkl_user_quest.quest_id) AS rq ON true
                          left join tkl_quest on rq = tkl_quest.id
