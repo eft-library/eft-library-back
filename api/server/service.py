dc6e55a8badd4ac599f60307949f8ce66489f2a9
poeynus <poeynus@gmail.com>
2025-01-17 15:31:52 +0900

    비상

--- api/server/service.py
+++ api/server/service.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
 import subprocess
-from kafka import KafkaProducerService
-
-kafka_producer_service = KafkaProducerService()
+# from kafka import KafkaProducerService
+#
+# kafka_producer_service = KafkaProducerService()
 load_dotenv()
 
 
@@ -13,12 +13,12 @@ class ServerService:
         subprocess.Popen([os.getenv("BUILD_PATH")], shell=True)
         return True
 
-    @staticmethod
-    async def save_footprint():
-        try:
-            await kafka_producer_service.send_message("asd")
-            return True
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return False
+    # @staticmethod
+    # async def save_footprint():
+    #     try:
+    #         await kafka_producer_service.send_message("asd")
+    #         return True
+    #     except Exception as e:
+    #         print(f"An error occurred: {e}")
+    #         return False
 
