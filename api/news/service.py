9b37c7407494dfebf3892f11c27cf7bb4752b50b
poeynus <poeynus@gmail.com>
2025-03-11 11:52:58 +0900

    초기화 정렬 추가

--- api/news/service.py
+++ api/news/service.py
@@ -1,6 +1,6 @@
 from api.news.models import News, Wipe
 from database import DataBaseConnector
-
+from sqlalchemy import desc
 
 class NewsService:
     @staticmethod
@@ -19,7 +19,7 @@ class NewsService:
         try:
             session = DataBaseConnector.create_session_factory()
             with session() as s:
-                wipe = s.query(Wipe).all()
+                wipe = s.query(Wipe).order_by(desc(Wipe.season_start)).all()
                 return wipe
         except Exception as e:
             print("오류 발생:", e)
