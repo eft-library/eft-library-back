047ee57073085caf5bfc7a7fc78617a4f7282d11
poeynus <poeynus@gmail.com>
2025-05-18 15:04:34 +0900

    map 정렬 수정

--- api/map_of_tarkov/util.py
+++ api/map_of_tarkov/util.py
@@ -3,26 +3,42 @@ class MapOfTarkovUtil:
     @staticmethod
     def get_map_of_tarkov_detail_query():
         return """
-            SELECT
-                a.id,
-                a.name,
-                a.three_image,
-                a.parent_value,
-                a.three_item_path,
-                a.jpg_image,
-                a.jpg_item_path,
-                a.depth,
-                a."order",
-                a.link,
-                a.mot_image,
-                a.map_json,
-                a.update_time,
-                jsonb_agg(b.*) FILTER (WHERE b.id IS NOT NULL) AS children
+            SELECT a.id,
+                   a.name,
+                   a.three_image,
+                   a.parent_value,
+                   a.three_item_path,
+                   a.jpg_image,
+                   a.jpg_item_path,
+                   a.depth,
+                   a."order",
+                   a.link,
+                   a.mot_image,
+                   a.map_json,
+                   a.update_time,
+                   (SELECT jsonb_agg(
+                                   jsonb_build_object(
+                                           'id', b.id,
+                                           'name', b.name,
+                                           'three_image', b.three_image,
+                                           'parent_value', b.parent_value,
+                                           'three_item_path', b.three_item_path,
+                                           'jpg_image', b.jpg_image,
+                                           'jpg_item_path', b.jpg_item_path,
+                                           'depth', b.depth,
+                                           'order', b."order",
+                                           'link', b.link,
+                                           'mot_image', b.mot_image,
+                                           'map_json', b.map_json,
+                                           'update_time', b.update_time
+                                   )
+                                   ORDER BY b."order"
+                           )
+                    FROM map_group_i18n b
+                    WHERE b.parent_value = a.id) AS children
             FROM map_group_i18n a
-            LEFT JOIN map_group_i18n b
-                ON b.parent_value = a.id
-            WHERE a.depth = 1 
-            and a.id = :map_id
+            WHERE a.depth = 1
+              AND a.id = :map_id
             GROUP BY a.id;
         """
 
