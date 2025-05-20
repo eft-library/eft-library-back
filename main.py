dcd7b0b41d749051cb06f6ff126d42be044f86ff
poeynus <poeynus@gmail.com>
2025-05-20 10:29:03 +0900

    ip 추적 테스트

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ app = FastAPI(title="eft-library-back")
 @app.middleware("http")
 async def kafka_producer_middleware(request: Request, call_next):
     now_kst = datetime.now(ZoneInfo("Asia/Seoul"))
-    client_ip = request.headers.get("x-forwarded-for", "").split(",")[0].strip()
-    print("X-Forwarded-For:", client_ip)
+    # client_ip = request.headers.get("x-forwarded-for", "").split(",")[0].strip()
+    print("X-Forwarded-For:", request.headers.get("x-forwarded-for", ""))
     footprint_time = now_kst.isoformat()
     data = {
         "method": request.method,
