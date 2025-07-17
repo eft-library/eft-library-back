9be8da3454ebb6971acfafea7a34c7bfb655ac93
poeynus <poeynus@gmail.com>
2025-07-17 13:19:21 +0900

    map of tarkov 예전 api 복구

--- api/map_of_tarkov/map_of_tarkov_router.py
+++ api/map_of_tarkov/map_of_tarkov_router.py
@@ -65,3 +65,12 @@ def get_find_info(map_id: str):
             None, HTTPCode.OK, Message.MAP_OF_TARKOV_NOT_FOUND
         )
     return CustomResponse.response(map_of_tarkov, HTTPCode.OK, Message.SUCCESS)
+
+@router.get("/detail/{map_id}")
+def get_map_of_tarkov(map_id: str):
+    map_of_tarkov = MapOfTarkovService.get_map_of_tarkov(map_id)
+    if map_of_tarkov is None:
+        return CustomResponse.response(
+            None, HTTPCode.OK, Message.MAP_OF_TARKOV_NOT_FOUND
+        )
+    return CustomResponse.response(map_of_tarkov, HTTPCode.OK, Message.SUCCESS)
--- api/map_of_tarkov/service.py
+++ api/map_of_tarkov/service.py
@@ -88,3 +88,55 @@ class MapOfTarkovService:
         except Exception as e:
             print("오류 발생:", e)
             return None
+    @staticmethod
+    def get_map_of_tarkov(map_id):
+        """
+        map of tarkov 지도 조회
+        """
+        try:
+            session = DataBaseConnector.create_session_factory()
+            with session() as s:
+                map_selector = (
+                    s.query(Map).filter(Map.depth == 1).order_by(Map.order).all()
+                )
+                map_selector_list = [{"id": m.id, "name": m.name} for m in map_selector]
+
+                map_query = text(MapOfTarkovUtil.get_map_of_tarkov_detail_query())
+                map_param = {"map_id": map_id}
+                map_result = s.execute(map_query, map_param)
+                map_data = [dict(row) for row in map_result.mappings()]
+
+                boss_query = text(MapOfTarkovUtil.get_map_of_tarkov_boss_query())
+                boss_param = {"map_id": map_id}
+                boss_result = s.execute(boss_query, boss_param)
+                boss_data = [dict(row) for row in boss_result.mappings()]
+
+                extraction_info = (
+                    s.query(Extraction)
+                    .filter(Extraction.map == map_id)
+                    .order_by(Extraction.faction, Extraction.name)
+                    .all()
+                )
+                transits_info = (
+                    s.query(Transits)
+                    .filter(Transits.map == map_id)
+                    .order_by(Transits.faction, Transits.name)
+                    .all()
+                )
+
+                find_info = s.query(WhereAmI).filter(WhereAmI.id == map_id).first()
+
+            map_of_tarkov = {
+                "map_info": map_data[0],
+                "extraction_info": extraction_info,
+                "transits_info": transits_info,
+                "map_id": map_id,
+                "find_info": find_info,
+                "map_selector": map_selector_list,
+                "boss_info": boss_data,
+            }
+
+            return map_of_tarkov
+        except Exception as e:
+            print("오류 발생:", e)
+            return None
