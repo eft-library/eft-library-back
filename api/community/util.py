fd615243f2390f52d95ab6eba1c0ba8fbd83a631
poeynus <poeynus@gmail.com>
2025-08-16 07:51:37 +0900

    상세 리턴 변경

--- api/community/util.py
+++ api/community/util.py
@@ -9,6 +9,7 @@ class CommunityUtil:
                 cp.user_email,
                 ui.nickname,
                 cp.category,
+                (select CASE WHEN uf.following_email = :user_email THEN 1 ELSE 0 END from user_follows uf) as is_follow,
                 cp.title,
                 cp.contents,
                 cp.thumbnail,
