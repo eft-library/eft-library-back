699a6a2348d099183e149bea9c7d8999fb0b433d
poeynus <poeynus@gmail.com>
2025-08-17 09:09:49 +0900

    게시글 상세 작성자 정보 추가

--- api/community/service.py
+++ api/community/service.py
@@ -165,16 +165,16 @@ class CommunityService:
                 result_issue_posts = CommunityFunction.fetch_issue_posts(s)
                 notice_posts = CommunityFunction.fetch_notice_posts(s)
 
-                author_meta_data_query = text(
-                    CommunityUtil.get_detail_author_meta_data()
-                )
-                author_meta_data_param = {"post_id": post_id, "user_email": user_email}
-                author_meta_data_result = s.execute(
-                    author_meta_data_query, author_meta_data_param
-                )
-                author_meta_data = [
-                    dict(row) for row in author_meta_data_result.mappings()
-                ]
+                # author_meta_data_query = text(
+                #     CommunityUtil.get_detail_author_meta_data()
+                # )
+                # author_meta_data_param = {"post_id": post_id, "user_email": user_email}
+                # author_meta_data_result = s.execute(
+                #     author_meta_data_query, author_meta_data_param
+                # )
+                # author_meta_data = [
+                #     dict(row) for row in author_meta_data_result.mappings()
+                # ]
 
                 # 게시글 목록 추가해야 함 - 이전이 이슈인지 카테고리인지 파악후 리턴을 해야 하는데 흠
 
