35d82c0e5d19f9eb4163b4407a1789e9b93e8a67
poeynus <poeynus@gmail.com>
2025-08-19 07:22:38 +0900

    comment router 추가

--- api/router.py
+++ api/router.py
@@ -20,6 +20,7 @@ from api.dashboard import dashboard_router
 from api.home import home_router
 from api.menu import menu_router
 from api.community import community_router
+from api.comment import comment_router
 
 api_router = APIRouter()
 
@@ -44,3 +45,4 @@ api_router.include_router(notice_router.router, prefix="/notice")
 api_router.include_router(price_router.router, prefix="/price")
 api_router.include_router(dashboard_router.router, prefix="/dashboard")
 api_router.include_router(community_router.router, prefix="/community")
+api_router.include_router(comment_router.router, prefix="/comment")
