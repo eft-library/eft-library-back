dd14c76f726bd16fd3ce2821462d6bbc94af03c3
poeynus <poeynus@gmail.com>
2025-09-04 07:26:11 +0900

    kafka 경로 수정 및 별도 파일로 분리

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-import json
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 from api.router import api_router
 from fastapi.openapi.docs import get_swagger_ui_html
-from kafka_producer import produce_message
-from fastapi import FastAPI, Request
-from datetime import datetime
-from zoneinfo import ZoneInfo
-import json
+from fastapi import FastAPI
+from util.middleware import KafkaProducerMiddleware
 
 load_dotenv()
 
@@ -24,30 +20,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-@app.middleware("http")
-async def kafka_producer_middleware(request: Request, call_next):
-    # 기존 Kafka 메시지 생산 로직
-    now_kst = datetime.now(ZoneInfo("Asia/Seoul"))
-    footprint_time = now_kst.isoformat()
-    data = {
-        "method": request.method,
-        "link": request.url.path,
-        "footprint_time": footprint_time,
-    }
-    json_str = json.dumps(data)
-    produce_message(json_str)
-
-    # 요청 처리 후 응답 가져오기
-    response = await call_next(request)
-
-    # iframe 허용을 위해 x-frame-options 헤더 제거 또는 변경
-    if "x-frame-options" in response.headers:
-        del response.headers["x-frame-options"]
-    if "X-Frame-Options" in response.headers:
-        del response.headers["X-Frame-Options"]
-
-    return response
+app.middleware(KafkaProducerMiddleware)
 
 
 @app.get("/docs")
---
+++ util/middleware.py
@@ -0,0 +1,26 @@
+import json
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from starlette.middleware.base import BaseHTTPMiddleware
+from util.kafka_producer import produce_message
+
+
+class KafkaProducerMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        now_kst = datetime.now(ZoneInfo("Asia/Seoul"))
+        footprint_time = now_kst.isoformat()
+        data = {
+            "method": request.method,
+            "link": request.url.path,
+            "footprint_time": footprint_time,
+        }
+        json_str = json.dumps(data)
+        produce_message(json_str)
+
+        response = await call_next(request)
+
+        for header in ("x-frame-options", "X-Frame-Options"):
+            if header in response.headers:
+                del response.headers[header]
+
+        return response
