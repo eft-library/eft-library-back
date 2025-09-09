9dcedb90f8d1f75a314deb649cf4ce53c34a3e5b
poeynus <poeynus@gmail.com>
2025-09-10 07:31:09 +0900

    알람 쿼리 수정

--- api/user/util.py
+++ api/user/util.py
@@ -305,7 +305,6 @@ class UserUtil:
             select user_email, noti_type, payload, is_read, created_time 
             from user_notifications un
             where un.user_email = :user_email
-            and is_read = false
             order by un.created_time desc  
             limit :limit
             offset :offset             
@@ -316,8 +315,7 @@ class UserUtil:
         return """
             select count(*)
             from user_notifications un
-            where un.user_email = :user_email
-            and is_read = false        
+            where un.user_email = :user_email        
         """
 
     @staticmethod
