ee958bf4bffc1b2e13dc94139be5730c918fd80a
poeynus <poeynus@gmail.com>
2025-11-15 08:05:09 +0900

    메인에 이슈 게시글 추가

--- api/home/service.py
+++ api/home/service.py
@@ -1,8 +1,10 @@
 from sqlalchemy.orm import subqueryload
 
+from api.community.util import CommunityUtil
 from api.dynamic_info.models import DynamicInfo
 from api.home.models import MenuGroup, MainInfo
 from database import DataBaseConnector
+from sqlalchemy import text
 
 
 class MenuService:
@@ -20,6 +22,16 @@ class MenuService:
                     .order_by(MenuGroup.order)
                     .all()
                 )
+
+                side_issue_posts_query = text(CommunityUtil.get_posts_with_issue())
+                get_side_issue_posts = s.execute(
+                    side_issue_posts_query,
+                    {"limit": 5, "offset": 0, "user_email": None},
+                )
+                get_side_issue_posts_data = [
+                    dict(row) for row in get_side_issue_posts.mappings()
+                ]
+
                 news_data = (
                     s.query(DynamicInfo).filter(DynamicInfo.id == "NEWS_COLUMN").first()
                 )
@@ -27,6 +39,8 @@ class MenuService:
                 main_info['main_info'] = main_info_list
                 main_info['menu'] = main_menu_list
                 main_info['news'] = news_data
+                main_info['issue_posts'] = get_side_issue_posts_data
+
                 return main_info
         except Exception as e:
             print("오류 발생:", e)
