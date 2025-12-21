9649220acd90f048e1b5d51c803a9b29474b22f2
poeynus <poeynus@gmail.com>
2025-12-21 10:03:15 +0900

    최신글 로직 수정

--- api/community/util.py
+++ api/community/util.py
@@ -295,9 +295,7 @@ class CommunityUtil:
         return """
         select cp.id::text AS id, cp.slug, cp.user_email, cp.category, cp.title
         from community_posts cp
-        where cp.user_email in ('poeynus@gmail.com', 'hjstudio0325@gmail.com', 'moonjipsa@gmail.com')
-        and cp.category = 'free'
-        and cp.delete_by_user = false
+        where cp.delete_by_user = false
         and cp.delete_by_admin = false
         order by cp.create_time desc
         limit 5        
