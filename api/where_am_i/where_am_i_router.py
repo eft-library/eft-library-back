2995b96038eb69fc717be7997433e907f2f550d5
poeynus <poeynus@gmail.com>
2025-12-21 12:07:30 +0900

    로그 추가

--- api/where_am_i/where_am_i_router.py
+++ api/where_am_i/where_am_i_router.py
@@ -18,6 +18,8 @@ def check_wpf_user(user: CheckWpfUser):
 
 @router.post("/send-location")
 async def send_location(where_am_i: ReqWhereAmI):
+    print(where_am_i.email)
+    print(where_am_i.location)
     result = await WhereAmIService.send_location(where_am_i)
     if result is None:
         return CustomResponse.response(None, HTTPCode.OK, Message.INVALID_USER)
