0d79131f1e441a76a630937c4b31ab338800bd5c
poeynus <poeynus@gmail.com>
2025-12-23 08:03:30 +0900

    오류 수정

--- api/where_am_i/res_models.py
+++ api/where_am_i/res_models.py
@@ -3,6 +3,7 @@ from sqlalchemy import (
     Column,
     TIMESTAMP,
     TEXT,
+    func,
 )
 
 
@@ -13,4 +14,4 @@ class UserLocationRequest(DataBaseConnector.Base):
     id = Column(TEXT, primary_key=True)
     user_email = Column(TEXT)
     location = Column(TEXT)
-    create_time = Column(TIMESTAMP)
+    create_time = Column(TIMESTAMP, server_default=func.now())
