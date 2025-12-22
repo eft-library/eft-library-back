738521dce5cb0d3c76a32eda652807a43556b6b4
poeynus <poeynus@gmail.com>
2025-12-23 08:05:49 +0900

    오류 수정

--- api/where_am_i/res_models.py
+++ api/where_am_i/res_models.py
@@ -1,17 +1,12 @@
 from database import DataBaseConnector
-from sqlalchemy import (
-    Column,
-    TIMESTAMP,
-    TEXT,
-    func,
-)
+from sqlalchemy import Column, TIMESTAMP, TEXT, func, INTEGER
 
 
 class UserLocationRequest(DataBaseConnector.Base):
 
     __tablename__ = "user_location_request"
 
-    id = Column(TEXT, primary_key=True)
+    id = Column(INTEGER, primary_key=True)
     user_email = Column(TEXT)
     location = Column(TEXT)
     created_time = Column(TIMESTAMP, server_default=func.now())
