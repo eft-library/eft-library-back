bef6e6b066bf6f2c747e4876b8f3f49a72840833
poeynus <poeynus@gmail.com>
2026-01-15 10:13:56 +0900

    은신처 inraid 아이템 표시 추가

--- api/hideout/util.py
+++ api/hideout/util.py
@@ -133,10 +133,10 @@ class HideoutUtil:
                         ELSE hiri.quantity
                     END
                 ) AS quantity,
-                hiri.found_in_raid,
-                hiri.image
+                hiri.image,
+                hiri.found_in_raid
             FROM hideout_item_require_i18n hiri
             LEFT JOIN user_hideout uh ON uh.user_email = :user_email
             WHERE hiri.item_id not in ('5449016a4bdc2d6f028b456f', '569668774bdc2da2298b4568', '5696686a4bdc2da3298b456a')
-            GROUP BY hiri.item_id, hiri.name, hiri.image;
+            GROUP BY hiri.item_id, hiri.name, hiri.image, hiri.found_in_raid;
         """
