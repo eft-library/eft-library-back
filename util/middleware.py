f22495d0c947b86f0db37f0cb97c356953f120d3
poeynus <poeynus@gmail.com>
2026-01-29 10:33:29 +0900

    middleware 수정

--- util/middleware.py
+++ util/middleware.py
@@ -8,6 +8,11 @@ import logging
 
 class KafkaProducerMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
+        print("=== All Headers ===")
+        for key, value in request.headers.items():
+            print(f"{key}: {value}")
+        print(f"request.client: {request.client}")
+        print("==================")
         now_kst = datetime.now(ZoneInfo("Asia/Seoul"))
         footprint_time = now_kst.isoformat()
 
