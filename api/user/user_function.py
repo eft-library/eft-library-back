28af488f8c5cd300915356beab84199ab17b9f5a
poeynus <poeynus@gmail.com>
2026-02-13 08:29:29 +0900

    회원가입 로직 수정

--- api/user/user_function.py
+++ api/user/user_function.py
@@ -73,8 +73,16 @@ class UserFunction:
 
     @staticmethod
     def _create_user_related_entries(session, email: str):
+        exists = session.query(UserQuest).filter(UserQuest.user_email == email).first()
+
+        # 이미 존재
+        if exists:
+            return
+
         new_user_quest = UserQuest(
-            user_email=email, quest_list=[], update_time=datetime.now()
+            user_email=email,
+            quest_list=[],
+            update_time=datetime.now(),
         )
         session.add(new_user_quest)
 
